737e92119f328ff7a209b28f5b85a46ba297d408
najmulazka <najmulazka225@gmail.com>
2023-11-04 00:21:00 +0700

    adjust profile routes

--- routes/profiles.routes.js
+++ routes/profiles.routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { updateProfile, detailProfile } = require('../controllers/profiles.controllers');
 const { image } = require('../libs/multer');
 
-router.get('/detailProfile', detailProfile);
+router.get('/', detailProfile);
 router.put('/', image.single('profile_picture'), updateProfile);
 
 module.exports = router;
